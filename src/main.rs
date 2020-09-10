c643c94ce592e5a43e21211eaab95bfe03707d40
tauraamui <adamplewis@pm.me>
2020-09-10 19:38:30 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use bevy::prelude::*;
+use bevy_obj::ObjPlugin;
+
+fn main() {
+    App::build()
+        .add_resource(Msaa { samples: 4 })
+        .add_default_plugins()
+        .add_plugin(ObjPlugin)
+        .add_startup_system(setup.system())
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // add entities to the world
+    commands
+        // mesh
+        .spawn(PbrComponents {
+            // load a mesh from binary glTF
+            mesh: asset_server
+                .load("res/person.obj")
+                .unwrap(),
+            // create a material for the mesh
+            material: materials.add(Color::rgb(0.5, 0.4, 0.3).into()),
+            translation: Translation::new(1.5, 0.0, 0.0),
+            ..Default::default()
+        })
+        // light
+        .spawn(LightComponents {
+            translation: Translation::new(4.0, 5.0, 4.0),
+            ..Default::default()
+        })
+        // camera
+        .spawn(Camera3dComponents {
+            transform: Transform::new_sync_disabled(Mat4::face_toward(
+                Vec3::new(-2.0, 2.0, 6.0),
+                Vec3::new(0.0, 0.0, 0.0),
+                Vec3::new(0.0, 1.0, 0.0),
+            )),
+            ..Default::default()
+        });
+}
+
