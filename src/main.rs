e14cd3388524d72cb132aceee606d6bc3808cd62
tauraamui <adamplewis@pm.me>
2020-09-11 20:50:51 +0100

    Add a ground plain to the world

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,10 @@ fn main() {
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+
     // add entities to the world
     commands
         // mesh
@@ -26,6 +28,12 @@ fn setup(
             // create a material for the mesh
             material: materials.add(Color::rgb(0.5, 0.4, 0.3).into()),
             translation: Translation::new(1.5, 0.0, 0.0),
+            ..Default::default()
+        })
+        .spawn(PbrComponents {
+            mesh: meshes.add(Mesh::from(shape::Plane { size: 10.0 })),
+            material: materials.add(Color::rgb(0.87, 0.92, 0.94).into()),
+            translation: Translation::new(0.0, 0.1, 0.0),
             ..Default::default()
         })
         // light
